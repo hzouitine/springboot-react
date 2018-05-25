6f6d67d2bd7860ec219b5f7844b3fa2d704e64f5
hzouitine <3zouitine@gmail.com>
2018-05-25 10:44:32 +0000

    Adding main component App, and fetching data in ComponentDidMount

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,1 +1,36 @@
-console.log("hzouitine!!!");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log("hzouitine");
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            employees: ''
+        };
+    }
+
+    componentDidMount() {
+        let response;
+        fetch("http://localhost:8080/api/employees")
+            .then(function (response) {
+                    return response.json();
+                }
+            )
+            .then((response) => {
+                this.setState((prevState) => ({employees: JSON.stringify(response)}));
+            });
+
+
+    }
+
+    render() {
+        return (<div>{this.state.employees}</div>
+            //<EmployeeList employees={this.state.employees}/>
+        )
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('react'));
